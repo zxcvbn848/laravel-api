f275231865c3407525f72b5520910d87f316e59e
Andy Wang <zxcvbn884488@gmail.com>
2023-04-28 01:03:35 +0800

    feat: use rules for validation

--- app/GraphQL/Schemas/DefaultSchema.php
+++ app/GraphQL/Schemas/DefaultSchema.php
@@ -3,26 +3,28 @@
 namespace App\GraphQL\Schemas;
 
 use App\GraphQL\{
-    Mutations\UpdateUserPasswordMutation,
-    Queries\UsersQuery,
-    Types\UserType,
+	Mutations\UpdateUserEmailMutation,
+	Mutations\UpdateUserPasswordMutation,
+	Queries\UsersQuery,
+	Types\UserType,
 };
 use Rebing\GraphQL\Support\Contracts\ConfigConvertible;
 
 class DefaultSchema implements ConfigConvertible
 {
-    public function toConfig(): array
-    {
-        return [
-            'query' => [
-                UsersQuery::class,
-            ],
-            'mutation' => [
-                UpdateUserPasswordMutation::class,
-            ],
-            'types' => [
-                UserType::class,
-            ],
-        ];
-    }
+	public function toConfig(): array
+	{
+		return [
+			'query' => [
+				UsersQuery::class,
+			],
+			'mutation' => [
+				UpdateUserPasswordMutation::class,
+				UpdateUserEmailMutation::class,
+			],
+			'types' => [
+				UserType::class,
+			],
+		];
+	}
 }
