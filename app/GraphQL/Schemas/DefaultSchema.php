6d95ec792f3226bb74fc2fa7cf277e6af29d0c97
Andy Wang <zxcvbn884488@gmail.com>
2023-04-27 23:54:59 +0800

    feat: define user schema and query

--- app/GraphQL/Schemas/DefaultSchema.php
+++ app/GraphQL/Schemas/DefaultSchema.php
@@ -2,6 +2,8 @@
 
 namespace App\GraphQL\Schemas;
 
+use App\GraphQL\Queries\UsersQuery;
+use App\GraphQL\Types\UserType;
 use Rebing\GraphQL\Support\Contracts\ConfigConvertible;
 
 class DefaultSchema implements ConfigConvertible
@@ -10,13 +12,13 @@ class DefaultSchema implements ConfigConvertible
     {
         return [
             'query' => [
-                ExampleQuery::class,
+                UsersQuery::class,
             ],
             'mutation' => [
                 ExampleMutation::class,
             ],
             'types' => [
-            
+                UserType::class,
             ],
         ];
     }
